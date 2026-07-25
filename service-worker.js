92a3325e954b20693f1728c4d30702de135d23ca
Dionvs <88378182+Dionvs@users.noreply.github.com>
2026-07-25 18:54:22 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "finize-v47-visual-rollback";
+const CACHE_NAME = "finize-v44-layout-and-expense-fix";
 
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./update4.js?v=47",
-  "./update4.css?v=47",
+  "./update4.js?v=44",
+  "./update4.css?v=44",
   "./update5.js?v=29",
   "./update5.css?v=29",
   "./finize-v4.html",
